cbfde0248701b709a7a40f14c7a00001b15acdbf
Evan Playle <playleed@eckerd.edu>
2019-05-28 16:34:35 +0100

    added topic/board view routing, views, and tests

--- djangoTutorial/boards/views.py
+++ djangoTutorial/boards/views.py
@@ -1,8 +1,23 @@
 #from django.shortcuts import render
 
 from django.shortcuts import render
+from django.http import Http404
 from .models import Board
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+
+""" def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board}) """
+
+
+def board_topics(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request, 'topics.html', {'board': board})
+
