441ed81404e7b71764bfddfb8854c651f220e02f
Evan Playle <playleed@eckerd.edu>
2019-06-06 17:08:05 +0100

    start of signup page and form render

--- djangoTutorial/accounts/views.py
+++ djangoTutorial/accounts/views.py
@@ -1,3 +1,9 @@
+
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+
+def signup(request):
+    form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
 # Create your views here.
