a2b204dcc67eaf9aaf4e612be22596cc87d2a1ae
Evan Playle <playleed@eckerd.edu>
2019-07-11 10:14:17 +0100

    Start of Password Reset

--- djangoTutorial/boards/tests/test_views.py
+++ djangoTutorial/boards/tests/test_views.py
@@ -5,9 +5,9 @@
 from django.contrib.auth.models import User
 from django.urls import resolve, reverse
 from django.test import TestCase
-from .views import home, board_topics, new_topic
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
 
 class HomeTests(TestCase):
     def setUp(self):
