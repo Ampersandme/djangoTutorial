83b3da517e738273bd625cceea35261bbbd5656e
Evan Playle <playleed@eckerd.edu>
2019-07-18 10:32:35 +0100

    Completed Password Change/Reset

--- djangoTutorial/MyApplication/urls.py
+++ djangoTutorial/MyApplication/urls.py
@@ -25,8 +25,7 @@ urlpatterns = [
     url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),   
-    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+
     url(r'^reset/$',
         auth_views.PasswordResetView.as_view(
             template_name='password_reset.html',
@@ -42,6 +41,12 @@ urlpatterns = [
         name='password_reset_confirm'),
     url(r'^reset/complete/$',
         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
-        name='password_reset_complete'),
+        name='password_reset_complete'), 
+    url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+        name='password_change'),
+    url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+        name='password_change_done'),
+    url(r'boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),   
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
 ]
